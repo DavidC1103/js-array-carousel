e422ed6f0f8ca733cd602510281265be143484be
David Calaiò <davidcjr11@gmail.com>
2023-03-26 14:22:44 +0200

    add img

---
+++ js/script..js
@@ -0,0 +1,7 @@
+const images = [
+    'assets-slider/img/img1.jpg',
+    'assets-slider/img/img2.jpg',
+    'assets-slider/img/img3.jpg',
+    'assets-slider/img/img4.jpg',
+    'assets-slider/img/img5.jpg',
+]
